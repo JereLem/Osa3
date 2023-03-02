c58a237a67833271bb6e83b865aab389f9c944fd
JereLem <56977360+JereLem@users.noreply.github.com>
2023-03-02 17:51:33 +0200

    step1-6

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
+const { request } = require('express')
 const express = require('express')
 const app = express()
 const port  = 3001
 
-const persons = [
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+
+
+let persons = [
   { id: 1, name: 'Arto hellas', number: '040-123456' },
   { id: 2, name: 'Ada Lovelace', number: '39-44-5323523' },
   { id: 3, name: 'Dan Abramov', number: '12-43-234345' },
@@ -14,8 +20,62 @@ app.get('/', (req, res) => {
 })
 
 app.get('/api/persons', (req, res) => {
-  res.json(persons)
+  res.json(persons);
+});
+
+app.post('/api/persons', (req, res) => {
+  const body = req.body;
+  
+  if (!body.name || !body.number) {
+    return res.status(400).json({ error: 'Name or number missing' });
+  }
+  
+  const nameExists = persons.some(person => person.name === body.name);
+  if (nameExists) {
+    return res.status(400).json({ error: 'Name already exists in the phonebook' });
+  }
+  
+  const person = {
+    id: Math.floor(Math.random() * 1000000),
+    name: body.name,
+    number: body.number
+  };
+  
+  persons = persons.concat(person);
+  
+  res.json(person);
+});
+
+
+app.get('/api/persons/:id', (req, res) => {
+  const id = req.params.id;
+  const index = parseInt(id) - 1;
+  if (isNaN(index) || index < 0 || index >= persons.length) {
+    res.status(404).end()
+  } else {
+    res.json(persons[index]);
+  }
+});
+
+app.delete('/api/persons/:id', (req, res) => {
+  const id = req.params.id;
+  const index = parseInt(id) - 1;
+  if (isNaN(index) || index < 0 || index >= persons.length) {
+    res.status(404).end()
+  } else {
+    persons = persons.filter(person => person.id !== id.to)
+    res.status(204).end()
+  }
+});
+
+
+
+app.get('/api/info', (req, res) => {
+  var count = Object.keys(persons).length;
+  var info = `The phonebook has info for ${count} people. ${new Date().toString()}`;
+  res.send(info);
 })
+
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`)
